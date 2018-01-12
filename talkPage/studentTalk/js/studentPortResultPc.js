59352cb07f97c829863814b5fc1e7bd63178c62e
wangyanxin <johndoe@example.com>
2018-01-12 15:50:43 +0800

    修改报告

--- talkPage/studentTalk/js/studentPortResultPc.js
+++ talkPage/studentTalk/js/studentPortResultPc.js
@@ -69,7 +69,6 @@ myActivityChart.setOption(pieOption);
 //综合分数环形图 end
 
 
-
 //雷达图 start
 var myChartRadarBox = echarts.init(document.getElementById('reportCanvas1'));
 var option = {
@@ -273,7 +272,7 @@ var option =
         "series": [{
             "type": "sankey",
             "layout": "none",
-            "layoutIterations":0,
+            "layoutIterations": 0,
             /*"data": [{
                 "name": "2016A等级"
             }, {
@@ -287,7 +286,7 @@ var option =
             }, {
                 "name": "2016C等级"
             }],*/
-            "data":[{
+            "data": [{
                 "name": "一年级"
             }, {
                 "name": "二年级"
@@ -545,10 +544,8 @@ var option = {
         data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
         z: 10
     },
-    radiusAxis: {
-    },
-    polar: {
-    },
+    radiusAxis: {},
+    polar: {},
     series: [{
         type: 'bar',
         data: [1, 2, 3, 4, 3, 5, 1],
@@ -591,8 +588,7 @@ for (var i = 0; i < 10; i++) {
     data4.push((Math.random() + 0.3).toFixed(2));
 }
 var itemStyle = {
-    normal: {
-    },
+    normal: {},
     emphasis: {
         barBorderWidth: 1,
         shadowBlur: 10,
@@ -697,14 +693,14 @@ myChartLiLang.setOption(option);
 //柔韧 start
 var myChartRouRen = echarts.init(document.getElementById('myChartRouRen'));
 var option = {
-    tooltip : {
+    tooltip: {
         trigger: 'axis',
-        axisPointer : {            // 坐标轴指示器，坐标轴触发有效
-            type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+        axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+            type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
         }
     },
     legend: {
-        data:['利润', '支出', '收入']
+        data: ['利润', '支出', '收入']
     },
     grid: {
         left: '3%',
@@ -712,44 +708,44 @@ var option = {
         bottom: '3%',
         containLabel: true
     },
-    xAxis : [
+    xAxis: [
         {
-            type : 'value'
+            type: 'value'
         }
     ],
-    yAxis : [
+    yAxis: [
         {
-            type : 'category',
-            axisTick : {show: false},
-            data : ['周一','周二','周三','周四','周五','周六','周日']
+            type: 'category',
+            axisTick: {show: false},
+            data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
         }
     ],
-    series : [
+    series: [
         {
-            name:'利润',
-            type:'bar',
+            name: '利润',
+            type: 'bar',
             label: {
                 normal: {
                     show: true,
                     position: 'inside'
                 }
             },
-            data:[200, 170, 240, 244, 200, 220, 210]
+            data: [200, 170, 240, 244, 200, 220, 210]
         },
         {
-            name:'收入',
-            type:'bar',
+            name: '收入',
+            type: 'bar',
             stack: '总量',
             label: {
                 normal: {
                     show: true
                 }
             },
-            data:[320, 302, 341, 374, 390, 450, 420]
+            data: [320, 302, 341, 374, 390, 450, 420]
         },
         {
-            name:'支出',
-            type:'bar',
+            name: '支出',
+            type: 'bar',
             stack: '总量',
             label: {
                 normal: {
@@ -757,7 +753,7 @@ var option = {
                     position: 'left'
                 }
             },
-            data:[-120, -132, -101, -134, -190, -230, -210]
+            data: [-120, -132, -101, -134, -190, -230, -210]
         }
     ]
 };
@@ -768,14 +764,14 @@ myChartRouRen.setOption(option);
 //耐力 start
 var myChartNaiLi = echarts.init(document.getElementById('myChartNaiLi'));
 var option = {
-    tooltip : {
+    tooltip: {
         trigger: 'axis',
-        axisPointer : {            // 坐标轴指示器，坐标轴触发有效
-            type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+        axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+            type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
         }
     },
     legend: {
-        data: ['直接访问', '邮件营销','联盟广告','视频广告','搜索引擎']
+        data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎']
     },
     grid: {
         left: '3%',
@@ -783,12 +779,12 @@ var option = {
         bottom: '3%',
         containLabel: true
     },
-    xAxis:  {
+    xAxis: {
         type: 'value'
     },
     yAxis: {
         type: 'category',
-        data: ['周一','周二','周三','周四','周五','周六','周日']
+        data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
     },
     series: [
         {
@@ -867,7 +863,7 @@ var option = {
         trigger: 'axis'
     },
     legend: {
-        data:['最高气温','最低气温']
+        data: ['最高气温', '最低气温']
     },
     toolbox: {
         show: true,
@@ -881,10 +877,10 @@ var option = {
             saveAsImage: {}
         }
     },
-    xAxis:  {
+    xAxis: {
         type: 'category',
         boundaryGap: false,
-        data: ['周一','周二','周三','周四','周五','周六','周日']
+        data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
     },
     yAxis: {
         type: 'value',
@@ -894,9 +890,9 @@ var option = {
     },
     series: [
         {
-            name:'最高气温',
-            type:'line',
-            data:[11, 11, 15, 13, 12, 13, 10],
+            name: '最高气温',
+            type: 'line',
+            data: [11, 11, 15, 13, 12, 13, 10],
             markPoint: {
                 data: [
                     {type: 'max', name: '最大值'},
@@ -910,9 +906,9 @@ var option = {
             }
         },
         {
-            name:'最低气温',
-            type:'line',
-            data:[1, -2, 2, 5, 3, 2, 0],
+            name: '最低气温',
+            type: 'line',
+            data: [1, -2, 2, 5, 3, 2, 0],
             markPoint: {
                 data: [
                     {name: '周最低', value: -2, xAxis: 1, yAxis: -1.5}
@@ -943,6 +939,69 @@ var option = {
 };
 myChartSuDu.setOption(option);
 //速度 end
+
+//结论 start
+var myChartJeLun = echarts.init(document.getElementById('myChartJeLun'));
+var hours = ['12a', '1a', '2a', '3a', '4a', '5a', '6a',
+    '7a', '8a', '9a', '10a', '11a',
+    '12p', '1p', '2p', '3p', '4p', '5p',
+    '6p', '7p', '8p', '9p', '10p', '11p'];
+var days = ['Saturday', 'Friday', 'Thursday',
+    'Wednesday', 'Tuesday', 'Monday', 'Sunday'];
+var data = [[0, 0, 5], [0, 1, 1], [0, 2, 0], [0, 3, 0], [0, 4, 0], [0, 5, 0], [0, 6, 0], [0, 7, 0], [0, 8, 0], [0, 9, 0], [0, 10, 0], [0, 11, 2], [0, 12, 4], [0, 13, 1], [0, 14, 1], [0, 15, 3], [0, 16, 4], [0, 17, 6], [0, 18, 4], [0, 19, 4], [0, 20, 3], [0, 21, 3], [0, 22, 2], [0, 23, 5], [1, 0, 7], [1, 1, 0], [1, 2, 0], [1, 3, 0], [1, 4, 0], [1, 5, 0], [1, 6, 0], [1, 7, 0], [1, 8, 0], [1, 9, 0], [1, 10, 5], [1, 11, 2], [1, 12, 2], [1, 13, 6], [1, 14, 9], [1, 15, 11], [1, 16, 6], [1, 17, 7], [1, 18, 8], [1, 19, 12], [1, 20, 5], [1, 21, 5], [1, 22, 7], [1, 23, 2], [2, 0, 1], [2, 1, 1], [2, 2, 0], [2, 3, 0], [2, 4, 0], [2, 5, 0], [2, 6, 0], [2, 7, 0], [2, 8, 0], [2, 9, 0], [2, 10, 3], [2, 11, 2], [2, 12, 1], [2, 13, 9], [2, 14, 8], [2, 15, 10], [2, 16, 6], [2, 17, 5], [2, 18, 5], [2, 19, 5], [2, 20, 7], [2, 21, 4], [2, 22, 2], [2, 23, 4], [3, 0, 7], [3, 1, 3], [3, 2, 0], [3, 3, 0], [3, 4, 0], [3, 5, 0], [3, 6, 0], [3, 7, 0], [3, 8, 1], [3, 9, 0], [3, 10, 5], [3, 11, 4], [3, 12, 7], [3, 13, 14], [3, 14, 13], [3, 15, 12], [3, 16, 9], [3, 17, 5], [3, 18, 5], [3, 19, 10], [3, 20, 6], [3, 21, 4], [3, 22, 4], [3, 23, 1], [4, 0, 1], [4, 1, 3], [4, 2, 0], [4, 3, 0], [4, 4, 0], [4, 5, 1], [4, 6, 0], [4, 7, 0], [4, 8, 0], [4, 9, 2], [4, 10, 4], [4, 11, 4], [4, 12, 2], [4, 13, 4], [4, 14, 4], [4, 15, 14], [4, 16, 12], [4, 17, 1], [4, 18, 8], [4, 19, 5], [4, 20, 3], [4, 21, 7], [4, 22, 3], [4, 23, 0], [5, 0, 2], [5, 1, 1], [5, 2, 0], [5, 3, 3], [5, 4, 0], [5, 5, 0], [5, 6, 0], [5, 7, 0], [5, 8, 2], [5, 9, 0], [5, 10, 4], [5, 11, 1], [5, 12, 5], [5, 13, 10], [5, 14, 5], [5, 15, 7], [5, 16, 11], [5, 17, 6], [5, 18, 0], [5, 19, 5], [5, 20, 3], [5, 21, 4], [5, 22, 2], [5, 23, 0], [6, 0, 1], [6, 1, 0], [6, 2, 0], [6, 3, 0], [6, 4, 0], [6, 5, 0], [6, 6, 0], [6, 7, 0], [6, 8, 0], [6, 9, 0], [6, 10, 1], [6, 11, 0], [6, 12, 2], [6, 13, 1], [6, 14, 3], [6, 15, 4], [6, 16, 0], [6, 17, 0], [6, 18, 0], [6, 19, 0], [6, 20, 1], [6, 21, 2], [6, 22, 2], [6, 23, 6]];
+data = data.map(function (item) {
+    return [item[1], item[0], item[2] || '-'];
+});
+var option = {
+    tooltip: {
+        position: 'top'
+    },
+    animation: false,
+    grid: {
+        height: '50%',
+        y: '10%'
+    },
+    xAxis: {
+        type: 'category',
+        data: hours,
+        splitArea: {
+            show: true
+        }
+    },
+    yAxis: {
+        type: 'category',
+        data: days,
+        splitArea: {
+            show: true
+        }
+    },
+    visualMap: {
+        min: 0,
+        max: 10,
+        calculable: true,
+        orient: 'horizontal',
+        left: 'center',
+        bottom: '15%'
+    },
+    series: [{
+        name: 'Punch Card',
+        type: 'heatmap',
+        data: data,
+        label: {
+            normal: {
+                show: true
+            }
+        },
+        itemStyle: {
+            emphasis: {
+                shadowBlur: 10,
+                shadowColor: 'rgba(0, 0, 0, 0.5)'
+            }
+        }
+    }]
+};
+myChartJeLun.setOption(option);
+//结论 end
 
 
 
